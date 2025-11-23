eabd7a2c2de0151c86edaa76df41de9dce368e19
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-11-23 09:41:12 -0800

    feat: add nixos module (#341)
    ---------
    
    Co-authored-by: Eduard Marbach <mail@eduard-marbach.de>

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Configarr flake";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    flake-parts,
+    nixpkgs,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      flake = {
+        nixosModules.default = ./pkgs/nix/module;
+      };
+
+      perSystem = {system, ...}: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = import ./pkgs/nix/package.nix {
+          inherit pkgs;
+          inherit (pkgs) lib;
+        };
+      };
+    };
+}
---
+++ pkgs/nix/package.nix
@@ -0,0 +1,62 @@
+{
+  lib,
+  pkgs,
+  fetchFromGitHub,
+  ...
+}:
+pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+  buildPhase = ''
+    runHook preBuild
+    pnpm build
+    runHook postBuild
+  '';
+
+  checkPhase = ''
+    runHook preCheck
+    pnpm test
+    runHook postCheck
+  '';
+
+  CI = "true";
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm644 -t $out/share bundle.cjs
+    makeWrapper ${lib.getExe pkgs.nodejs_24} $out/bin/configarr \
+      --add-flags "$out/share/bundle.cjs"
+    runHook postInstall
+  '';
+
+  meta = {
+    changelog = "https://github.com/raydak-labs/configarr/blob/${finalAttrs.src.rev}/CHANGELOG.md";
+    description = "Sync TRaSH Guides + custom configs with Sonarr/Radarr";
+    homepage = "https://github.com/raydak-labs/configarr";
+    license = lib.licenses.agpl3Only;
+    mainProgram = "configarr";
+    maintainers = with lib.maintainers; [lord-valen];
+    platforms = lib.platforms.all;
+  };
+
+  nativeBuildInputs = [
+    pkgs.makeBinaryWrapper
+    pkgs.nodejs_24
+    pkgs.pnpm.configHook
+  ];
+
+  pname = "configarr";
+
+  pnpmDeps = pkgs.pnpm.fetchDeps {
+    fetcherVersion = 1;
+    hash = "sha256-0P5gT29uLCmm10Xerk9ZVblEoauTEd9jzi0jseO3Ojc=";
+    inherit (finalAttrs) pname src version;
+  };
+
+  src = fetchFromGitHub {
+    owner = "raydak-labs";
+    repo = "configarr";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-fgv6wiK5wh0jAczJWy3Iqs3OK81ckNr3bOZD32bTCQQ=";
+  };
+
+  version = "1.17.2";
+})
